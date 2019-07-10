51c15e9e4bd743f966026e56f0a39167785797e0
John M. Wargo <johnwargo@users.noreply.github.com>
2019-07-10 18:50:22 -0400

    updated Wed 07/10/2019

--- tipcalc/sw.js
+++ tipcalc/sw.js
@@ -10,24 +10,22 @@
 // illustrate how to install it, I'm not going to do any app
 // file caching (although most PWAs would).
 
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
     // fires when the browser installs the app
     // here we're just logging the event and the contents
     // of the object passed to the event. the purpose of this event
     // is to give the service worker a place to setup the local 
     // environment after the installation completes.
-    console.log('Event: install');
-    console.dir(e);
-    // console.table(e);
+    console.log(`Event fired: ${event.type}`);
+    console.dir(event);
 });
 
 self.addEventListener('activate', event => {
     // fires after the service worker completes its installation. 
     // It's a place for the service worker to clean up from previous 
     // service worker versions
-    console.log('Event: activate');
+    console.log(`Event fired: ${e.type}`);
     console.dir(event);
-    // console.table(event);
 });
 
 self.addEventListener('fetch', event => {
@@ -39,7 +37,6 @@ self.addEventListener('fetch', event => {
     // any more about this here
     console.log('Event: fetch');
     console.dir(event);
-    // console.table(event);
     // Go get the requested resource from the network, nothing fancy
     // going on here.
     return fetch(event.request);
