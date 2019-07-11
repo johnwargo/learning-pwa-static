593d1cb487e6bd6734b827cabfbb81812e85ca44
John M. Wargo <johnwargo@users.noreply.github.com>
2019-07-10 20:02:04 -0400

    updated Wed 07/10/2019

--- tipcalc/sw.js
+++ tipcalc/sw.js
@@ -41,7 +41,7 @@ self.addEventListener('fetch', event => {
     // covering different cache strategies, so I'm not going to say 
     // any more about this here
     console.log(`Fetching ${event.request}`);
-    // console.dir(event);
+    console.dir(event.request);
     // Next, go get the requested resource from the network, 
     // nothing fancy going on here.
     return fetch(event.request);
